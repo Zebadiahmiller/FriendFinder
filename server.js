39534500a19336aa59b43c6076d97944d4cb223b
Zebadiah Miller <Zebadiahmiller@homtial.com>
2019-08-20 21:23:15 -0700

    chaning heroku ports

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require("path")
 
 const app = express();
 
-const port = process.env.PORT || 8080;
+const port = process.env.port || 8080;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
