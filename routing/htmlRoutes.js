2f6bd98222afdaa31381f2c56323b113803d1405
Zebadiah Miller <Zebadiahmiller@homtial.com>
2019-08-22 21:44:49 -0700

    updating routes

--- routing/htmlRoutes.js
+++ routing/htmlRoutes.js
@@ -2,17 +2,17 @@ const path = require("path");
 module.exports = function(app){
 
 app.get("/survey",  function (req, res){  
-res.sendfile(path.join(__dirname, "../public/survey.html"))
+res.sendfile(path.join(__dirname, "./public/survey.html"))
 });
 
 app.get("/", function(req,res) {
  
-    res.sendFile(path.join(__dirname, "../public/home.html"));
+    res.sendFile(path.join(__dirname, "./public/home.html"));
   });
 
 app.get("*", function(req,res) {
  
-    res.sendFile(path.join(__dirname, "../public/home.html"));
+    res.sendFile(path.join(__dirname, "./public/home.html"));
   });
 
 }
